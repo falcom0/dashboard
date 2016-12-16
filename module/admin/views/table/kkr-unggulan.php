0ab8185836cb3229f6ba63b4d8d45d0b996c8413
falcom0 <f.ichwan@gmail.com>
2016-12-17 01:31:39 +0700

    Revert to normal. To use crawling look into dump file

--- module/admin/views/table/kkr-unggulan.php
+++ module/admin/views/table/kkr-unggulan.php
@@ -1,79 +1,17 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-
 use app\assets\admin\table\KkrUnggulanAsset;
 use app\module\admin\widgets\TableWidget;
 use yii\helpers\VarDumper;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
-use keltstr\simplehtmldom\SimpleHTMLDom as SHD;
-use app\module\admin\widgets\LoginDss;
+
 KkrUnggulanAsset::register($this);
 
 $this->title = 'DATATABLE Dashboard USU';
-$login = new LoginDss('administrator', 'administrator');
 
-$html = SHD::str_get_html(
-	$login->getPage(
-		'https://dss.usu.ac.id/mahasiswa-terdaftar/berdasarkan-jenis-kelamin.html',
-		$semester
-	)
-);
-
-$result = $html->find('table[class=table_wrapper]', 0);
 ?>
-<section id='page-content'>
-<div class='header-content'> 
-                     <h2><i class='fa fa-table'></i>Indikator Capaian Keunggulan Akademik USU 2015-2019</h2> 
-               </div>
-<div class='body-content animated fadeIn'>
-        <div class='row'>
-            <div class='col-md-12'>
 
-                <!-- Start repeater -->
-                <div class='panel rounded shadow no-overflow'>
-                    <div class='panel-heading'>
-                        <div class='pull-left'>
-                            <h3 class='panel-title'>Test</h3>
-                        </div>
-                        <div class='pull-right'>
-                            <button class='btn btn-sm' data-action='refresh' data-container='body' data-toggle='tooltip' data-placement='top' data-title='Refresh'><i class='fa fa-refresh'></i></button>
-                            <button class='btn btn-sm' data-action='collapse' data-container='body' data-toggle='tooltip' data-placement='top' data-title='Collapse'><i class='fa fa-angle-up'></i></button>
-                            <button class='btn btn-sm' data-action='remove' data-container='body' data-toggle='tooltip' data-placement='top' data-title='Remove'><i class='fa fa-times'></i></button>
-                        </div>
-                        <div class='clearfix'></div>
-                    </div>
-<!-- <div id="crawltable"> -->
-<div class='panel-body'>
-                        <!-- Start repeater -->
-                        <div class='fuelux'>
-                        <div class='repeater-viewport'>
-                                    <div class='repeater-canvas'>
-                                   <div id="crawltable">
-<!--                                     <div class="repeater-list" data-preserve="shallow"> -->
-<!--                                     <div class="repeater-list-wrapper" data-infinite="true" data-preserve="shallow"> -->
-<!--                                     <table aria-readonly="true" class="table" data-container="true" data-preserve="shallow" role="grid"> -->
-    									<?php echo $result ?>
-    									</div>                                
-<!--     									</div> -->
-<!--     									</div> -->
-                                    </div>
-                                </div>
-	
-	</div>
-	</div>
-<!-- </div> -->
-</div><!-- /.panel -->
-                <!--/ End repeater -->
-
-               
-
-            </div><!-- /.col-md-12 -->
-        </div><!-- /.row -->
-
-    </div>
-</section>
 <!-- <section id='page-content'> -->
                 <!-- Start page header -->
 <!--                 <div class='header-content'> -->
@@ -103,5 +41,5 @@ $result = $html->find('table[class=table_wrapper]', 0);
 <!--          </select> -->
 <!--  	</div> -->
 <!--  </div> -->
-<!--   //TableWidget::widget(['title'=>'Indikator Capaian Keunggulan Akademik USU 2015-2019']) -->
+  <?=TableWidget::widget(['title'=>'Indikator Capaian Keunggulan Akademik USU 2015-2019'])?>
 <!-- </section> -->
